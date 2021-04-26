2fbbb6f254d96c137668fc40004730b93ef3a474
abu-nofal <anofal719@gmail.com>
2021-04-26 16:06:36 +0300

    add a new file and dir

---
+++ js/app.js
@@ -0,0 +1,64 @@
+'use strict';
+alert('hello , and welcome to my web introduction ');
+
+let userName=prompt('please enter your name ');
+
+alert('welcom '+ userName);
+
+
+
+let myName =prompt('what is my name is it ahmad? you can answer with \n yes or no ');
+if (myName.toUpperCase()==='YES'|| myName.toUpperCase()==='Y' ){
+    alert('great jop ');
+}
+else if(myName.toUpperCase()==='NO'|| myName.toUpperCase()==='N' ){
+    alert('ooops that is a wrong answer my name is ahmad');
+}
+
+
+
+
+
+let myAge=prompt('is my age 23 ?  ')
+if (myAge.toUpperCase ()==='YES'|| myAge.toUpperCase ()==='Y' ){
+    alert('great jop ');
+}
+else if(myAge.toUpperCase()==='NO'|| myAge.toUpperCase()==='N' ){
+    alert('ooops that is a wrong answer my age is 23');
+}
+
+
+
+
+
+let colorEye=prompt('what do you thanke about mu color eyes is it broan ?');
+if (colorEye.toUpperCase()==='YES'|| colorEye.toUpperCase()==='Y'){
+    alert('great jop ');
+}
+else if(colorEye.toUpperCase ()==='NO'|| colorEye.toUpperCase ()==='N'){
+    alert('ooops that is a wrong answer actually color of my eyes is broan');
+}
+
+
+
+
+let study=prompt('what do you thank about my mojor is it a mechanical engineer ?');
+
+if (study.toUpperCase() ==='YES'|| study.toUpperCase() ==='Y' ){
+    alert('great jop ');
+}
+else if(study.toUpperCase() ==='NO'||  study.toUpperCase() ==='N'){
+    alert('ooops that is a wrong answer my mojor is actually mechanical engineer ');
+}
+
+
+
+let phoneType=prompt('do i have an android phone ?');
+if (phoneType.toUpperCase()==='YES'|| phoneType.toUpperCase()==='Y' ){
+    alert('great jop ');
+}
+else if(phoneType.toUpperCase()==='NO'|| phoneType.toUpperCase()==='N' ){
+    alert('ooops that is a wrong answer i have an android deviece or phone');
+}
+
+alert('thank you ' + userName + ' for your time ,please return as many you want ' );
